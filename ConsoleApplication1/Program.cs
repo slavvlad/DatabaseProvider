bd0596c625b18239dd1c1ecf8d6e9f4b80b4230e
vlad <vladspamm@gmail.com>
2015-11-20 14:21:30 +0200

    Add select, update function

---
+++ ConsoleApplication1/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (var provider = MySqlProvider.Provider.CreateProvider())
+            {
+                provider.OpenConnection();
+                var result = provider.ExecuteSelectCommand("SELECT * FROM world.city;");
+                int rowEffectedCount = provider.ExecuteUpdateComand("update world.city set population=1000000 where id =1");
+            }
+        }
+    }
+}
---
+++ MySqlProvider/Provider.cs
@@ -0,0 +1,116 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MySql.Data.MySqlClient;
+using System.Data;
+using System.Configuration;
+
+namespace MySqlProvider
+{
+    public class Provider: IDisposable
+    {
+        private MySqlConnection m_conn;
+        private string m_connectionString;
+
+        public static Provider CreateProvider(string connectionString="")
+        {
+            if (string.IsNullOrEmpty(connectionString))
+            {
+                ConnectionStringSettingsCollection settings =
+                ConfigurationManager.ConnectionStrings;
+
+                if (settings != null)
+                {
+                    foreach (ConnectionStringSettings cs in settings)
+                    {
+                        if (cs.ProviderName == "MySql.Data.MySqlClient")
+                            return new Provider(cs.ConnectionString);
+                    }
+
+                }
+                throw new ArgumentNullException("connectionString", "Value have to assign");
+            }
+            return new Provider(connectionString);
+        }
+
+        private Provider(string connectionString)
+        {
+            this.m_connectionString = connectionString;
+        }
+
+        public void OpenConnection()
+        {
+            m_conn = new MySql.Data.MySqlClient.MySqlConnection();
+            m_conn.ConnectionString = m_connectionString;
+            m_conn.Open();
+        }
+
+        public DataTable ExecuteSelectCommand(string commandText)
+        {
+            if (m_conn.State != System.Data.ConnectionState.Open)
+                m_conn.Open();
+            MySqlCommand cmd = new MySqlCommand(commandText, m_conn);
+
+            MySqlDataAdapter MyAdapter = new MySqlDataAdapter();
+            MyAdapter.SelectCommand = cmd;
+            DataTable dTable = new DataTable();
+            MyAdapter.Fill(dTable);
+            return dTable;
+            //MySqlDataReader reader = cmd.ExecuteReader();
+            //while (reader.Read())
+            //{
+            //    //Console.WriteLn(reader[0], reader[1]...);
+            //}
+        }
+
+        public int ExecuteUpdateComand(string commandText)
+        {
+            if (m_conn.State != System.Data.ConnectionState.Open)
+                m_conn.Open();
+            MySqlCommand cmd = new MySqlCommand(commandText, m_conn);
+            using (MySqlDataReader reader = cmd.ExecuteReader())
+            {
+                return reader.RecordsAffected;
+            }
+        }
+
+        #region IDisposable Support
+        private bool disposedValue = false; // To detect redundant calls
+
+        protected virtual void Dispose(bool disposing)
+        {
+            if (!disposedValue)
+            {
+                if (disposing)
+                {
+                    // TODO: dispose managed state (managed objects).
+                }
+
+                m_conn.Close();
+                // TODO: free unmanaged resources (unmanaged objects) and override a finalizer below.
+                // TODO: set large fields to null.
+
+                disposedValue = true;
+            }
+        }
+
+        // TODO: override a finalizer only if Dispose(bool disposing) above has code to free unmanaged resources.
+        // ~Provider() {
+        //   // Do not change this code. Put cleanup code in Dispose(bool disposing) above.
+        //   Dispose(false);
+        // }
+
+        // This code added to correctly implement the disposable pattern.
+        public void Dispose()
+        {
+            // Do not change this code. Put cleanup code in Dispose(bool disposing) above.
+            Dispose(true);
+            // TODO: uncomment the following line if the finalizer is overridden above.
+            // GC.SuppressFinalize(this);
+        }
+        #endregion
+
+    }
+}
